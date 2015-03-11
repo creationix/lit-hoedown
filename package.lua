6e455b14d29d5b2ef26ccb1f91892f9a73d33c17
Tim Caswell <tim@creationix.com>
2015-03-11 16:58:55 -0500

    Update example

---
+++ package.lua
@@ -0,0 +1,12 @@
+return {
+  name = "creationix/hoedown",
+  version = "1.0.0",
+  homepage = "https://github.com/creationix/lit-hoedown",
+  files = {
+    "*.lua",
+    "*.h",
+    "!hoedown",
+    "!hoedown-sample",
+    "$OS-$ARCH/*",
+  }
+}
