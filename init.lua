6349fae9cbcc63d75b6da9d923dd8b9789186ceb
Tim Caswell <tim@creationix.com>
2015-03-12 00:27:24 -0500

    Start to add non-bundle support to hoedown

--- init.lua
+++ init.lua
@@ -1,14 +1,34 @@
 local ffi = require('ffi')
+local fs = require('fs')
 local bundle = require('luvi').bundle
 
 local pathJoin = require('luvi').path.join
 
 -- Get path
-local base = module.dir:gsub("^bundle:", "")
+local base = module.dir
+local isBundle = false
+if base:match("^bundle:") then
+  base = base:gsub("^bundle:", "")
+  isBundle = true
+end
 local dir = pathJoin(base, ffi.os .. "-" .. ffi.arch)
-local entries = bundle.readdir(dir)
+local entries
+if isBundle then
+  entries = bundle.readdir(dir)
+else
+  entries = fs.readdirSync(dir)
+end
 assert(entries and entries[1], "Missing shared library in : " .. dir)
-ffi.cdef(bundle.readfile(pathJoin(base, "hoedown.h")))
+
+local header
+local headerPath = pathJoin(base, "hoedown.h")
+if isBundle then
+  header = bundle.readfile(headerPath)
+else
+  header = fs.readFileSync(headerPath)
+end
+
+ffi.cdef(header)
 return bundle.action(pathJoin(dir, entries[1]), function (path)
   -- Load module
   return ffi.load(path)
