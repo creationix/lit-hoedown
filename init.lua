7620a8644ab49f8c7eafb8a6111fd2883330f731
Tim Caswell <tim@creationix.com>
2015-05-08 15:14:14 -0500

    Add metadata and migrate to module:* apis

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
-return require('ffi-loader')(module.dir, "hoedown.h")
+local names = {
+  ["Linux-arm"] = "libhoedown.so",
+  ["Linux-x64"] = "libhoedown.so",
+  ["OSX-x64"] = "libhoedown.dylib",
+}
+
+local ffi = require('ffi')
+ffi.cdef(module:load("hoedown.h"))
+
+local arch = ffi.os .. "-" .. ffi.arch
+return module:action(arch .. "/" .. names[arch], function (path)
+  return ffi.load(path)
+end)
--- package.lua
+++ package.lua
@@ -1,10 +1,11 @@
 return {
   name = "creationix/hoedown",
-  version = "1.0.7",
+  version = "1.1.0",
   homepage = "https://github.com/creationix/lit-hoedown",
-  dependencies = {
-    "creationix/ffi-loader@1.0.2"
-  },
+  description = "FFI bindings to the hoedown markdown library",
+  tags = {"ffi", "markdown", "codec"},
+  author = { name = "Tim Caswell" },
+  license = "MIT",
   files = {
     "*.lua",
     "*.h",
